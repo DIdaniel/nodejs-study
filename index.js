b661a4875cbe43d0bf851054acdf389f6f753baa
dream_Faunus <cappu159@gmail.com>
2021-04-11 19:58:39 +0900

    ID function create with Bcrypt

--- index.js
+++ index.js
@@ -27,12 +27,42 @@ app.post('/register', (req, res) => {
   // 회원가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 database에 넣어준다
   const user = new User(req.body)
+
   user.save((err, userInfo) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  // 1. 요청된 E-mail을 데이터베이스에 있는지 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false, 
+        message: "이메일에 해당하는 유저가 없습니다"
+      })
+    }
+    // 2. 요청된 E-mail이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인하기
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다" })
+
+      // 비밀번호까지 맞다면 TOKEN을 생성하기
+      user.generateToken((err, user) => {
+        
+      })
+    })
+    
+
+  })
+
+  
+
+  // 3. 비밀번호가 맞다면 Token 생성하기
+
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
   name: {
@@ -32,6 +35,36 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function( next ) {
+  var user = this;
+
+  if(user.isModified('password')) {
+    // 비밀번호를 암호화 시키기!!
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if(err) return next(err)
+
+          user.password = hash
+
+          next()
+      });
+    });  
+  }  else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  //plainPassword와 암호화된 비밀번호가 같은지 확인해야한다
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return callback(err),
+    //만약 두 암호가 같다면?
+    callback(null, isMatch)
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
